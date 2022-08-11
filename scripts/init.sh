2702f51bc87ac191ea47764fa51a284a580d5ffb
POeticPotatoes <poeticpotatoes@gmail.com>
2022-08-12 02:05:43 +0800

    Add NTU darkmode script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
-
-xrandr --output HDMI-0 --pos 0x0 --rotate right --brightness 0.9 --mode 1920x1080 --rate 75 --output DP-0 --primary --pos 1080x420 --rotate normal --mode 1920x1080 --rate 144
+if (( $(xrandr -q | grep -c " connected ") > 1 ));
+then
+    echo "More than one screen found. Formatting HDMI-0 and DP-0."
+    xrandr --output HDMI-0 --pos 0x0 --rotate right --brightness 0.9 --mode 1920x1080 --rate 75 --output DP-0 --primary --pos 1200x420 --rotate normal --mode 1920x1080 --rate 144
+else
+    echo "One screen found. Assuming a connection of DP-0."
+    xrandr --output DP-0 --primary --pos 1200x420 --rotate normal --mode 1920x1080 --rate 144
+fi
 nitrogen --restore
 
---
+++ scripts/ntu-stars-darkmode.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         NTU STARS Dark Mode
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  dark mode for NTU Star!
+// @author       POeticPotatoes (Check me out on Github!)
+// @match        https://wish.wis.ntu.edu.sg/pls/webexe/AUS_STARS*
+// @icon         https://static.vecteezy.com/system/resources/previews/002/387/838/original/calendar-icon-flat-style-isolated-on-white-background-free-vector.jpg
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+// Note: If there is a timetable clash, text will not appear red with this script enabled.
+
+// Step 1: Install the tampermonkey extension
+// Step 2: Create a new script and paste this ENTIRE file into the editor
+// Step 3: Save the file. Enjoy!
+
+GM_addStyle ( `
+    body, div, .site-footer{
+        background-color: #202226 !important;
+        font-family: sans-serif;
+        color: #fff !important;
+    }
+    font {
+        color: #fff !important;
+    }
+    td {
+        border: 1px solid #555 !important;
+        background-color: #202226 !important;
+    }
+    .site-logo{
+        display: none !important;
+    }
+    input {
+        background-color:#3d6599 !important;
+    }
+    a {
+        color: #24a7ed !important;
+    }
+` );
